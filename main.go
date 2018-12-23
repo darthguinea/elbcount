a7131740b59798abee53b0eaec86e14f8d4c6dd4
ade <darthguinea@protonmail.com>
2018-12-23 20:09:49 +1100

    Added watcher

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"os"
+	"strings"
+	"time"
 
 	"./src/cw"
 
@@ -20,14 +23,46 @@ func createSession(r string) *session.Session {
 func main() {
 	var (
 		flagVerbose bool
+		flagList    bool
+		flagWatch   bool
+		flagRegion  string
+		flagNames   string
 	)
 	flag.BoolVar(&flagVerbose, "v", false, "verbose")
+	flag.StringVar(&flagRegion, "r", "ap-southeast-2", "-r <region>")
+	flag.StringVar(&flagNames, "n", "", "-n <elb_name,elb_name,elb_name>")
+	flag.BoolVar(&flagList, "l", false, "-l list elbs in the region")
+	flag.BoolVar(&flagWatch, "w", false, "-w watch the request count")
 	flag.Parse()
 
 	if flagVerbose {
 		log.SetLevel(log.INFO)
 	}
 
-	session := createSession("ap-southeast-2")
-	cw.GetMetric(session)
+	session := createSession(flagRegion)
+
+	if flagList {
+		cw.List(session)
+		os.Exit(0)
+	}
+
+	elb := strings.Split(flagNames, ",")
+
+	startTime := time.Now().Add(time.Duration(-60) * time.Minute)
+	endTime := time.Now()
+
+	log.Print("Between [%v] -> [%v]",
+		startTime.Format("2006-1-2 15:04"),
+		endTime.Format("2006-1-2 15:04"))
+
+	for {
+		for _, elbName := range elb {
+			data := cw.GetMetric(session, elbName, &startTime, &endTime)
+			cw.DrawGraph(elbName, data)
+		}
+		if !flagWatch {
+			os.Exit(0)
+		}
+		time.Sleep(10 * time.Second)
+	}
 }
--- src/cw/cw.go
+++ src/cw/cw.go
@@ -1,44 +1,76 @@
 package cw
 
 import (
+	"os"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/guptarohit/asciigraph"
 
 	"github.com/darthguinea/golib/log"
 )
 
-func GetMetric(session *session.Session) {
+func List(session *session.Session) {
+	svc := elbv2.New(session)
+
+	op, err := svc.DescribeLoadBalancers(&elbv2.DescribeLoadBalancersInput{})
+	if err != nil {
+		log.Error("%v", err)
+		os.Exit(1)
+	}
+
+	log.Print("%35v - %v", "Short Name", "Name")
+	for _, elb := range op.LoadBalancers {
+		val := strings.Split(*elb.LoadBalancerArn, "/")
+		log.Print("%35v - %v", *elb.LoadBalancerName, log.Sprintf("%v/%v/%v", val[1], val[2], val[3]))
+	}
+}
+
+func GetMetric(session *session.Session,
+	name string,
+	startTime *time.Time, endTime *time.Time) []*float64 {
 	svc := cloudwatch.New(session)
 
-	startTime := time.Now().Add(time.Duration(-10) * time.Minute)
-	endTime := time.Now()
-
-	log.Print("Between [%v] -> [%v]", startTime.Format("2006-1-2 15:04"), endTime.Format("2006-1-2 15:04"))
-
-	period := int64(300)
-	filter := &cloudwatch.GetMetricDataInput{
-		StartTime: &startTime,
-		EndTime:   &endTime,
+	period := int64(60)
+	op, err := svc.GetMetricData(&cloudwatch.GetMetricDataInput{
+		StartTime: startTime,
+		EndTime:   endTime,
 		MetricDataQueries: []*cloudwatch.MetricDataQuery{{
+			Id: aws.String("test"),
 			MetricStat: &cloudwatch.MetricStat{
 				Metric: &cloudwatch.Metric{
 					Namespace:  aws.String("AWS/ApplicationELB"),
 					MetricName: aws.String("RequestCount"),
 					Dimensions: []*cloudwatch.Dimension{{
 						Name:  aws.String("LoadBalancer"),
-						Value: aws.String("<load balancer name>"),
+						Value: aws.String(name),
 					}},
 				},
 				Period: &period,
-				Stat:   aws.String("Average"),
+				Stat:   aws.String("Sum"),
 			},
 		}},
+	})
+	if err != nil {
+		log.Error("%v", err)
+		return nil
 	}
-	op, _ := svc.GetMetricData(filter)
+	return op.MetricDataResults[0].Values
+}
 
-	log.Print("%v", filter)
-	log.Print("%v", op)
+func DrawGraph(name string, x []*float64) {
+	data := []float64{}
+	for _, i := range x {
+		data = append(data, *i)
+	}
+	log.Print("%v", name)
+	if len(data) > 0 {
+		graph := asciigraph.Plot(data, asciigraph.Height(20))
+		log.Print("%v", graph)
+		log.Print("")
+	}
 }
